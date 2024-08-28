9886cdc3b35408fa2885fbb33b4058e3615caff4
Yohualkis <Yohualkis99@gmail.com>
2024-08-28 13:11:49 -0400

    Se creo la carpeta Models y el modelo Tecnico

---
+++ RegistroTecnico/Models/Tecnico.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Models
+{
+    public class Tecnico
+    {
+        [Key]
+        private int tecnicoId { get; set; }
+        private string? nombre { get; set; }
+        private double sueldoHora { get; set; }
+    }
+}
