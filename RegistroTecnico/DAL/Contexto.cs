701dba4b6f419a042a88fbde70fde294f06854fd
Yohualkis <Yohualkis99@gmail.com>
2024-08-28 13:11:15 -0400

    Se creo la carpeta DAL y el Contexto

---
+++ RegistroTecnico/DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnico.Models;
+
+namespace RegistroTecnico.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) 
+            : base(options) { }
+
+        public DbSet<Tecnico> Tecnicos { get; set; }
+    }
+}
