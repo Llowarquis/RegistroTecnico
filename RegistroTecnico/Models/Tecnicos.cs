fb5b10ee8eab61610098ef30621fb52164f41036
Yohualkis <Yohualkis99@gmail.com>
2024-09-02 20:15:10 -0400

    Se agrego el required, el file scoped namespaces y la "s" a la clase Tecnico

---
+++ RegistroTecnico/Models/Tecnicos.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Models;
+public class Tecnicos
+{
+    [Key]
+    public int TecnicoId { get; set; }
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public string? Nombres { get; set; }
+    public double SueldoHora { get; set; }
+}
