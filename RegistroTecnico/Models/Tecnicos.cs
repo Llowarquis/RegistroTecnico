9060467f6a6bbee07c0e4835b2121d8e0c432201
Yohualkis <Yohualkis99@gmail.com>
2024-09-06 15:19:57 -0400

    Se agrego una expresion regular para el atributo "Nombres"

--- RegistroTecnico/Models/Tecnicos.cs
+++ RegistroTecnico/Models/Tecnicos.cs
@@ -7,7 +7,8 @@ public class Tecnicos
     [Key]
     public int TecnicoId { get; set; }
     [Required(ErrorMessage = "Este campo es obligatorio.")]
+    [RegularExpression(@"^[a-zA-Z-ÁáÉéÍíÓóÚúÑñ\s]+$", ErrorMessage = "Este campo solo puede alojar letras/espacios.")]
     public string? Nombres { get; set; }
-    
+
     public double SueldoHora { get; set; }
 }
