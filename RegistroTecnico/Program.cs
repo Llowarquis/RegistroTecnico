71be3744114dc952f3ca42d4c8c14c8259292ca9
Yohualkis <Yohualkis99@gmail.com>
2024-09-06 13:20:48 -0400

    File Scoped Namespace fixed

--- RegistroTecnico/Program.cs
+++ RegistroTecnico/Program.cs
@@ -3,50 +3,48 @@ using RegistroTecnico.Components;
 using RegistroTecnico.DAL;
 using RegistroTecnico.Services;
 
-namespace RegistroTecnico
+namespace RegistroTecnico;
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents();
+
+        // Obtencion del connection string
+        var ConStr = builder.Configuration.GetConnectionString("ConStr");
+
+        // Inyeccion del ConStr
+        builder.Services.AddDbContext<Contexto>(o => o.UseSqlite(ConStr));
+
+        // Inyeccion del servicio
+        builder.Services.AddScoped<TecnicoServices>();
+
+
+
+
+        // De aqui pa'rriba
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
-                .AddInteractiveServerComponents();
-
-            // Obtencion del connection string
-            var ConStr = builder.Configuration.GetConnectionString("ConStr");
-
-            // Inyeccion del ConStr
-            builder.Services.AddDbContext<Contexto>(o => o.UseSqlite(ConStr));
-
-            // Inyeccion del servicio
-            builder.Services.AddScoped<TecnicoServices>();
-
-
-
-
-            // De aqui pa'rriba
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseStaticFiles();
-            app.UseAntiforgery();
-
-            app.MapRazorComponents<App>()
-                .AddInteractiveServerRenderMode();
-
-            app.Run();
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+
+        app.UseStaticFiles();
+        app.UseAntiforgery();
+
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode();
+
+        app.Run();
     }
 }
