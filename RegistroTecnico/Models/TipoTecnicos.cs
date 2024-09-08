7a4545db2bf0ac2409b6edd33920c58e5e1493a5
Yohualkis <Yohualkis99@gmail.com>
2024-09-08 12:33:56 -0400

    Se creo el modelo "TipoTecnicos"

---
+++ RegistroTecnico/Models/TipoTecnicos.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Models;
+public class TipoTecnicos
+{
+    [Key]
+    public int TipoTencicoId { get; set; }
+    [Required]
+    public string? Descripcion { get; set; }
+}
