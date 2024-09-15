811feb63d7e730549d5ce335d104c90abe9c974d
Yohualkis <Yohualkis99@gmail.com>
2024-09-15 16:38:26 -0400

    Se crearon los Service correspondientes para los modelos Clientes y Trabajos

---
+++ RegistroTecnico/Services/ClientesService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnico.DAL;
+using RegistroTecnico.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnico.Services;
+
+public class ClientesService(Contexto contexto)
+{
+    // Se hace uso del primary constructor
+    private readonly Contexto _contexto = contexto;
+
+    // Este metodo se utilizara en a la hora de modificar o insertar un empleado
+    // ambas pantallas presentaran un boton tipo guardar
+    public async Task<bool> Guardar(Clientes cliente)
+    {
+        if (!await Existe(cliente.ClienteId))
+            return await Insertar(cliente);
+        else
+            return await Modificar(cliente);
+    }
+
+    private async Task<bool> Existe(int id)
+    {
+        return await _contexto.Clientes
+            .AnyAsync<Clientes>(t => t.ClienteId == id);
+    }
+
+    private async Task<bool> Insertar(Clientes cliente)
+    {
+        _contexto.Clientes.Add(cliente);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+
+    private async Task<bool> Modificar(Clientes cliente)
+    {
+        _contexto.Clientes.Update(cliente);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Eliminar(int id)
+    {
+        return await _contexto.Clientes
+            .Where(t => t.ClienteId == id)
+            .AsNoTracking()
+            .ExecuteDeleteAsync() > 0;
+    }
+
+    public async Task<Clientes?> Buscar(int id)
+    {
+        return await _contexto.Clientes
+            .AsNoTracking()
+            .FirstOrDefaultAsync(t => t.ClienteId == id);
+    }
+
+    public async Task<List<Clientes>> Listar(Expression<Func<Clientes, bool>> criterio)
+    {
+        return await _contexto.Clientes
+            .AsNoTracking()
+            .Where(criterio)
+            .ToListAsync();
+    }
+
+    public async Task<bool> ExisteNombre(int clienteId, string? name)
+    {
+        return await _contexto?.Clientes
+            .AnyAsync(e => e.ClienteId != clienteId
+            && e.Nombres.ToLower().Equals(name.ToLower()));
+    }
+}
---
+++ RegistroTecnico/Services/TrabajosService.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnico.DAL;
+using RegistroTecnico.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnico.Services;
+
+public class TrabajosService(Contexto contexto)
+{
+    // Se hace uso del primary constructor
+    private readonly Contexto _contexto = contexto;
+
+    // Este metodo se utilizara en a la hora de modificar o insertar un empleado
+    // ambas pantallas presentaran un boton tipo guardar
+    public async Task<bool> Guardar(Trabajos trabajo)
+    {
+        if (!await Existe(trabajo.TrabajosId))
+            return await Insertar(trabajo);
+        else
+            return await Modificar(trabajo);
+    }
+
+    private async Task<bool> Existe(int id)
+    {
+        return await _contexto.Trabajos
+            .AnyAsync<Trabajos>(tra => tra.TrabajosId == id);
+    }
+
+    private async Task<bool> Insertar(Trabajos trabajo)
+    {
+        _contexto.Trabajos.Add(trabajo);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+
+    private async Task<bool> Modificar(Trabajos trabajo)
+    {
+        _contexto.Trabajos.Update(trabajo);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<bool> Eliminar(int id)
+    {
+        return await _contexto.Trabajos
+            .Where(t => t.TrabajosId == id)
+            .AsNoTracking()
+            .ExecuteDeleteAsync() > 0;
+    }
+
+    public async Task<Trabajos?> Buscar(int id)
+    {
+        return await _contexto.Trabajos
+            .Include(t => t.Tecnico)
+            .Include(c => c.Cliente)
+            .AsNoTracking()
+            .FirstOrDefaultAsync(t => t.TrabajosId == id);
+    }
+
+    public async Task<List<Trabajos>> Listar(Expression<Func<Trabajos, bool>> criterio)
+    {
+        return await _contexto.Trabajos
+            .Include(t => t.Tecnico)
+            .Include(c => c.Cliente)
+            .AsNoTracking()
+            .Where(criterio)
+            .ToListAsync();
+    }
+
+    public async Task<bool> ExisteDescripcion( string? descripcion)
+    {
+        return await _contexto?.Trabajos
+            .AnyAsync(e => e.Descripcion.ToLower().Equals(descripcion.ToLower()));
+    }
+}
