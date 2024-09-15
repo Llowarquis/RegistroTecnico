5dcea1623b32208565b1e4712257558ce86a0fcf
Yohualkis <Yohualkis99@gmail.com>
2024-09-15 16:30:17 -0400

    Se crearon los modelos Clientes y Trabajos

---
+++ RegistroTecnico/Models/Clientes.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Models;
+public class Clientes
+{
+	[Key]
+	public int ClienteId { get; set; }
+
+
+	[RegularExpression(@"^[a-zA-Z-ÁáÉéÍíÓóÚúÑñ\s]+$", ErrorMessage = "Este campo solo puede alojar letras/espacios.")]
+	[Required(ErrorMessage = "Este campo es obligatorio.")]
+	public string? Nombres { get; set; }
+
+
+	[RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Este campo solo puede alojar numeros.")]
+	[Required(ErrorMessage = "Este campo es obligatorio.")]
+	public string? WhatsApp { get; set; }
+}
