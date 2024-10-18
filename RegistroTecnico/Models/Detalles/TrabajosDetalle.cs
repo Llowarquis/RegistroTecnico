07e76ad757fef11fbedb01cd66c3d53c95e4537e
Yohualkis <Yohualkis99@gmail.com>
2024-10-18 09:04:15 -0400

    Se creo el detalle del modelo Trabajos

---
+++ RegistroTecnico/Models/Detalles/TrabajosDetalle.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Models.Detalles;
+
+public class TrabajosDetalle
+{
+	[Key]
+	public int DetalleId { get; set; }
+
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public Trabajos? TrabajoId { get; set; }
+
+
+	[Required(ErrorMessage = "Este campo es obligatorio.")]
+	public Articulos? ArticuloId { get; set; }
+
+
+	[Required(ErrorMessage = "Este campo es obligatorio.")]
+	public int Cantidad { get; set; }
+
+
+	[Required(ErrorMessage = "Este campo es obligatorio.")]
+	public double Precio { get; set; }
+
+
+	public double Costo { get; set; } // Este campo estara oculto a la vista del usuario
+}
