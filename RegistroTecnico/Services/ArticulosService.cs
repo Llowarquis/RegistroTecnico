453617b2384e382f4609d303b9c5bf761998242d
Yohualkis <Yohualkis99@gmail.com>
2024-10-18 09:04:41 -0400

    Se creo el servicio del modelo Articulos

---
+++ RegistroTecnico/Services/ArticulosService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnico.DAL;
+using RegistroTecnico.Models;
+using System.Linq.Expressions;
+
+namespace RegistroTecnico.Services;
+
+public class ArticulosService(Contexto contexto)
+{
+	private readonly Contexto _contexto = contexto;
+
+	// Listar
+	public async Task<List<Articulos>> Listar(Expression<Func<Articulos, bool>> criterio)
+	{
+		return await _contexto.Articulos
+			.AsNoTracking()
+			.Where(criterio)
+			.ToListAsync();
+	}
+}
