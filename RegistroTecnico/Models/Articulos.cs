2487dfb54856d18fb8efd3fa102272d6b455482b
Yohualkis <Yohualkis99@gmail.com>
2024-10-18 09:04:26 -0400

    Se creo el modelo Articulos

---
+++ RegistroTecnico/Models/Articulos.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnico.Models;
+
+public class Articulos 
+{ 
+	/*
+	 * Aqui no pondre ningun DataAnnotationValidator porque en el contexto
+	 * ya existe un OnModelCreating
+	 */
+
+    [Key]
+    public int ArticuloId { get; set; }
+
+    public string? Descripcion { get; set; }
+
+	public double Costo { get; set; }
+
+	public double Precio { get; set; }
+
+	public bool Existencia { get; set; }
+}
