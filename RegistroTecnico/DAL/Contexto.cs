db651efab5ee7e871fdd3d7be52818b4eac11c3d
Yohualkis <Yohualkis99@gmail.com>
2024-10-21 04:44:06 -0400

    Se aagrego OnModelCreating para no crear otro regsitro de articulos

--- RegistroTecnico/DAL/Contexto.cs
+++ RegistroTecnico/DAL/Contexto.cs
@@ -5,7 +5,7 @@ using RegistroTecnico.Models.Detalles;
 namespace RegistroTecnico.DAL;
 public class Contexto : DbContext
 {
-    public Contexto(DbContextOptions<Contexto> options) 
+    public Contexto(DbContextOptions<Contexto> options)
         : base(options) { }
 
     public DbSet<Tecnicos> Tecnicos { get; set; }
@@ -16,15 +16,16 @@ public class Contexto : DbContext
     public DbSet<Articulos> Articulos { get; set; }
     public DbSet<TrabajosDetalle> TrabajosDetalles { get; set; }
 
-	protected override void OnModelCreating(ModelBuilder modelBuilder)
-	{
-		base.OnModelCreating(modelBuilder);
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
         modelBuilder.Entity<Articulos>().HasData(new List<Articulos>()
         {
-            new Articulos() { ArticuloId = 1, Descripcion = "Punta de eje", Existencia = true, Precio = 4999.99, Costo = 2999.99},
-			new Articulos() { ArticuloId = 2, Descripcion = "Cremallera", Existencia = true, Precio = 7999.99, Costo = 3999.99},
-			new Articulos() { ArticuloId = 3, Descripcion = "Junta de motor", Existencia = true, Precio = 2549.99, Costo = 959.99},
-			new Articulos() { ArticuloId = 4, Descripcion = "Alternador", Existencia = true, Precio = 3699.99, Costo = 1599.99}
-		});
-	}
+            new Articulos() { ArticuloId = 1, Descripcion = "Memoria RAM", Existencia = 51, Precio = 4999.99, Costo = 2999.99},
+            new Articulos() { ArticuloId = 2, Descripcion = "MotherBoard", Existencia = 64, Precio = 7999.99, Costo = 3999.99},
+            new Articulos() { ArticuloId = 3, Descripcion = "Cables UTP Cat6 (500ft)", Existencia = 85, Precio = 2549.99, Costo = 959.99},
+            new Articulos() { ArticuloId = 4, Descripcion = "Power Supply", Existencia = 71, Precio = 3699.99, Costo = 1599.99},
+            new Articulos() { ArticuloId = 5, Descripcion = "Cpu", Existencia = 84, Precio = 9599.50, Costo = 4769.80}
+        });
+    }
 }
