c210da96b61f5e9203cfd66fa4f3a1e7b4cf86f3
Yohualkis <Yohualkis99@gmail.com>
2024-10-21 04:46:19 -0400

    #

--- RegistroTecnico/Services/TrabajosDetalleService.cs
+++ RegistroTecnico/Services/TrabajosDetalleService.cs
@@ -1,5 +1,21 @@
-﻿namespace RegistroTecnico.Services;
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroTecnico.DAL;
+using RegistroTecnico.Models;
+using RegistroTecnico.Models.Detalles;
+using System.Linq.Expressions;
 
-public class TrabajosDetalleService
+namespace RegistroTecnico.Services;
+
+public class TrabajosDetalleService(Contexto contexto)
 {
+	private readonly Contexto _contexto = contexto;
+
+	// Listar
+	public async Task<List<TrabajosDetalle>> Listar(Expression<Func<TrabajosDetalle, bool>> criterio)
+	{
+		return await _contexto.TrabajosDetalles
+			.AsNoTracking()
+			.Where(criterio)
+			.ToListAsync();
+	}
 }
