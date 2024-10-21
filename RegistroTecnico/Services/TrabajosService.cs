6d02dd60163f7253f90039021cb2aed016b7bc63
Yohualkis <Yohualkis99@gmail.com>
2024-10-21 04:47:15 -0400

    Se modificaron los metodos modificar, insertar, eliminar y listar. Se agrego el metodo AfectarArticulo

--- RegistroTecnico/Services/TrabajosService.cs
+++ RegistroTecnico/Services/TrabajosService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using RegistroTecnico.DAL;
 using RegistroTecnico.Models;
+using RegistroTecnico.Models.Detalles;
 using System.Linq.Expressions;
 
 namespace RegistroTecnico.Services;
@@ -26,32 +27,46 @@ public class TrabajosService(Contexto contexto)
             .AnyAsync<Trabajos>(tra => tra.TrabajosId == id);
     }
 
-    private async Task<bool> Insertar(Trabajos trabajo)
-    {
-        _contexto.Trabajos.Add(trabajo);
-        return await _contexto.SaveChangesAsync() > 0;
-    }
+	private async Task<bool> Insertar(Trabajos trabajos)
+	{
 
-    private async Task<bool> Modificar(Trabajos trabajo)
-    {
-        _contexto.Trabajos.Update(trabajo);
-        return await _contexto.SaveChangesAsync() > 0;
-    }
+		await AfectarArticulo(trabajos.TrabajoDetalle.ToArray(), true);
+		_contexto.Trabajos.Add(trabajos);
+		return await _contexto.SaveChangesAsync() > 0;
+	}
 
-    public async Task<bool> Eliminar(int id)
+	private async Task<bool> Modificar(Trabajos trabajos)
+	{
+		var trabajoOriginal = await _contexto.Trabajos
+		                        .Include(t => t.TrabajoDetalle)
+		                        .AsNoTracking()
+		                        .FirstOrDefaultAsync(t => t.TrabajosId == trabajos.TrabajosId);
+
+		await AfectarArticulo(trabajoOriginal.TrabajoDetalle.ToArray(), false);
+		await AfectarArticulo(trabajos.TrabajoDetalle.ToArray(), true);
+
+        _contexto.Attach(trabajos);
+		return await _contexto.SaveChangesAsync() > 0;
+	}
+
+	public async Task<bool> Eliminar(int id)
     {
-        return await _contexto.Trabajos
-            .Where(t => t.TrabajosId == id)
-            .AsNoTracking()
-            .ExecuteDeleteAsync() > 0;
-    }
+		var trabajos = _contexto.Trabajos.Find(id);
+		await AfectarArticulo(trabajos.TrabajoDetalle.ToArray(), false);
+
+		_contexto.TrabajosDetalles.RemoveRange(trabajos.TrabajoDetalle);
+		_contexto.Trabajos.Remove(trabajos);
+
+		var cantidad = await _contexto.SaveChangesAsync();
+        return cantidad > 0;
+	}
 
     public async Task<Trabajos?> Buscar(int id)
     {
         return await _contexto.Trabajos
             .Include(t => t.Tecnico)
             .Include(c => c.Cliente)
-            .AsNoTracking()
+            .Include(td => td.TrabajoDetalle)
             .FirstOrDefaultAsync(t => t.TrabajosId == id);
     }
 
@@ -60,14 +75,28 @@ public class TrabajosService(Contexto contexto)
         return await _contexto.Trabajos
             .Include(t => t.Tecnico)
             .Include(c => c.Cliente)
+            .Include(p => p.Prioridad)
+            .Include(td => td.TrabajoDetalle)
             .AsNoTracking()
             .Where(criterio)
             .ToListAsync();
     }
 
-    public async Task<bool> ExisteDescripcion( string? descripcion)
-    {
-        return await _contexto?.Trabajos
-            .AnyAsync(e => e.Descripcion.ToLower().Equals(descripcion.ToLower()));
-    }
+    //public async Task<bool> ExisteDescripcion( string? descripcion)
+    //{
+    //    return await _contexto?.Trabajos
+    //        .AnyAsync(e => e.Descripcion.ToLower().Equals(descripcion.ToLower()));
+    //} 
+
+	private async Task AfectarArticulo(TrabajosDetalle[] detalle, bool resta = true)
+	{
+		foreach (var item in detalle)
+		{
+			var Articulo = await _contexto.Articulos.SingleAsync(p => p.ArticuloId == item.ArticuloId);
+			if (resta)
+				Articulo.Existencia -= item.Cantidad;
+			else
+				Articulo.Existencia += item.Cantidad;
+		}
+	}
 }
