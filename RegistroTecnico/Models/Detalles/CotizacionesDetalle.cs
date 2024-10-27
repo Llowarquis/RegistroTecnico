aba63e4893b894fb0b7197464148fcf011f6aa97
Yohualkis <Yohualkis99@gmail.com>
2024-10-26 23:06:48 -0400

    Se creo el modelo CotizacionesDetalle

---
+++ RegistroTecnico/Models/Detalles/CotizacionesDetalle.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnico.Models.Detalles;
+
+public class CotizacionesDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    [ForeignKey("Cotizaciones")]
+    public int CotizacionId { get; set; }
+    public Cotizaciones? Cotizacion { get; set; }
+
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    [ForeignKey("Articulos")]
+    public int ArticuloId { get; set; }
+    public Articulos? Articulos { get; set; }
+
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public int Cantidad { get; set; }
+
+
+    [Required(ErrorMessage = "Este campo es obligatorio.")]
+    public double Precio { get; set; }
+}
