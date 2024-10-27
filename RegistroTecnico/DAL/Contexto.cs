008216ad376cccf37b1be228fa0e81d63e09d33d
Yohualkis <Yohualkis99@gmail.com>
2024-10-26 23:05:38 -0400

    Se añadieorn los modelos Cotizaciones y CotizacionesDetalles

--- RegistroTecnico/DAL/Contexto.cs
+++ RegistroTecnico/DAL/Contexto.cs
@@ -15,6 +15,8 @@ public class Contexto : DbContext
     public DbSet<Prioridades> Prioridades { get; set; }
     public DbSet<Articulos> Articulos { get; set; }
     public DbSet<TrabajosDetalle> TrabajosDetalles { get; set; }
+    public DbSet<Cotizaciones> Cotizaciones { get; set; }
+    public DbSet<CotizacionesDetalle> CotizacionesDetalles { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
